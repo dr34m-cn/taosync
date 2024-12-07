1acba18678701a34f48e2026c5fa9865a723d4fa
dr34m-cn <1416814478@qq.com>
2024-12-07 15:22:04 +0800

    update  引擎管理&通知配置 加载中效果优化

--- frontend/src/views/page/engine/index.vue
+++ frontend/src/views/page/engine/index.vue
@@ -1,48 +1,52 @@
 <template>
-	<div class="engine" v-loading="getLoading">
-		<div class="card-item" v-for="item in alistList">
-			<div class="card-item-top">
-				<el-image src="/alist.svg" fit="contain" style="width: 60px;height: 60px;"></el-image>
-				<div style="margin-left: 12px;">
-					<div class="card-item-user">{{item.userName}}
-						<div class="card-item-remark" v-if="item.remark != null">[{{item.remark}}]</div>
+	<div class="engine">
+		<div class="loading-box content-none-data" v-loading="true" v-if="getLoading">加载中</div>
+		<div v-else class="card-box">
+			<div class="card-item" v-for="item in alistList">
+				<div class="card-item-top">
+					<el-image src="/alist.svg" fit="contain" style="width: 60px;height: 60px;"></el-image>
+					<div style="margin-left: 12px;">
+						<div class="card-item-user">{{item.userName}}
+							<div class="card-item-remark" v-if="item.remark != null">[{{item.remark}}]</div>
+						</div>
+						<div class="card-item-url">{{item.url}}</div>
 					</div>
-					<div class="card-item-url">{{item.url}}</div>
+				</div>
+				<div class="card-item-bottom">
+					<el-button size="small" type="primary" @click="editShowDialog(item)">编辑</el-button>
+					<el-button size="small" type="danger" @click="delAlist(item.id)">删除</el-button>
 				</div>
 			</div>
-			<div class="card-item-bottom">
-				<el-button size="small" type="primary" @click="editShowDialog(item)">编辑</el-button>
-				<el-button size="small" type="danger" @click="delAlist(item.id)">删除</el-button>
+			<div class="card-item card-add" @click="addShow" v-if="!getLoading">
+				<template v-if="alistList.length == 0">
+					暂无引擎，请<span style="color: #409eff;">新增</span>
+				</template>
+				<span v-else>新增</span>
 			</div>
+			<el-dialog :close-on-click-modal="false" :visible.sync="editShow" :title="editFlag ? '编辑' : '新增'" width="600px"
+				:before-close="closeShow" :append-to-body="true">
+				<div class="elform-box">
+					<el-form :model="editData" :rules="editFlag ? editRule : addRule" ref="addRule" v-if="editShow"
+						label-width="66px">
+						<el-form-item prop="url" label="地址">
+							<el-input v-model="editData.url" placeholder="请输入地址，如http://127.0.0.1:5244"></el-input>
+						</el-form-item>
+						<el-form-item prop="remark" label="备注">
+							<el-input v-model="editData.remark" placeholder="备注方便你标识引擎，非必填"></el-input>
+						</el-form-item>
+						<el-form-item prop="token" label="令牌">
+							<el-input v-model="editData.token" show-password
+								:placeholder="`请输入令牌，${editFlag ? '留空表示不修改' : '请到AList管理-设置-其他中复制，保存后不要重置令牌'}`"
+								@keyup.enter.native="submit"></el-input>
+						</el-form-item>
+					</el-form>
+				</div>
+				<span slot="footer" class="dialog-footer">
+					<el-button @click="closeShow">取 消</el-button>
+					<el-button type="primary" @click="submit" :loading="editLoading">确 定</el-button>
+				</span>
+			</el-dialog>
 		</div>
-		<div class="card-item card-add" @click="addShow" v-if="!getLoading">
-			<template v-if="alistList.length == 0">
-				暂无引擎，请<span style="color: #409eff;">新增</span>
-			</template>
-			<span v-else>新增</span>
-		</div>
-		<el-dialog :close-on-click-modal="false" :visible.sync="editShow" :title="editFlag ? '编辑' : '新增'" width="600px"
-			:before-close="closeShow" :append-to-body="true">
-			<div class="elform-box">
-				<el-form :model="editData" :rules="editFlag ? editRule : addRule" ref="addRule" v-if="editShow"
-					label-width="66px">
-					<el-form-item prop="url" label="地址">
-						<el-input v-model="editData.url" placeholder="请输入地址，如http://127.0.0.1:5244"></el-input>
-					</el-form-item>
-					<el-form-item prop="remark" label="备注">
-						<el-input v-model="editData.remark" placeholder="备注方便你标识引擎，非必填"></el-input>
-					</el-form-item>
-					<el-form-item prop="token" label="令牌">
-						<el-input v-model="editData.token" show-password :placeholder="`请输入令牌，${editFlag ? '留空表示不修改' : '请到AList管理-设置-其他中复制，保存后不要重置令牌'}`"
-							@keyup.enter.native="submit"></el-input>
-					</el-form-item>
-				</el-form>
-			</div>
-			<span slot="footer" class="dialog-footer">
-				<el-button @click="closeShow">取 消</el-button>
-				<el-button type="primary" @click="submit" :loading="editLoading">确 定</el-button>
-			</span>
-		</el-dialog>
 	</div>
 </template>
 
@@ -178,71 +182,84 @@
 <style lang="scss" scoped>
 	.engine {
 		box-sizing: border-box;
-		padding: 8px;
-		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(340px, 2fr));
 		width: 100%;
-		// height: 100%;
+		height: 100%;
 
-		.card-item {
-			background-color: #292b3c;
-			border-radius: 5px;
-			border: 1px solid;
-			border-color: transparent;
-			height: 110px;
-			margin: 8px;
-			padding: 6px;
+		.loading-box {
+			box-sizing: border-box;
+			width: 100%;
+			height: 100%;
+		}
 
-			.card-item-top {
-				display: flex;
-				align-items: center;
-				justify-content: center;
+		.card-box {
+			box-sizing: border-box;
+			padding: 8px;
+			display: grid;
+			grid-template-columns: repeat(auto-fill, minmax(340px, 2fr));
+			width: 100%;
 
-				.card-item-user {
-					font-size: 18px;
+			.card-item {
+				background-color: #292b3c;
+				border-radius: 5px;
+				border: 1px solid;
+				border-color: transparent;
+				height: 110px;
+				margin: 8px;
+				padding: 6px;
+
+				.card-item-top {
 					display: flex;
+					align-items: center;
+					justify-content: center;
 
-					.card-item-remark {
-						margin-left: 6px;
-						color: #d6d12f;
-						max-width: 120px;
-						white-space: nowrap;
-						overflow: hidden;
-						text-overflow: ellipsis;
+					.card-item-user {
+						font-size: 18px;
+						display: flex;
+
+						.card-item-remark {
+							margin-left: 6px;
+							color: #d6d12f;
+							max-width: 120px;
+							white-space: nowrap;
+							overflow: hidden;
+							text-overflow: ellipsis;
+						}
+					}
+
+					.card-item-url {
+						margin-top: 8px;
+						font-size: 12px;
 					}
 				}
 
-				.card-item-url {
-					margin-top: 8px;
-					font-size: 12px;
+				.card-item-bottom {
+					display: flex;
+					align-items: center;
+					justify-content: center;
+					margin-top: 12px;
 				}
 			}
 
-			.card-item-bottom {
+			.card-add {
+				font-size: 32px;
+				cursor: pointer;
 				display: flex;
-				align-items: center;
 				justify-content: center;
-				margin-top: 12px;
+				align-items: center;
+			}
+
+			.card-item:hover {
+				border-color: #409eff;
+				background-color: #3d415a;
+			}
+
+			.card-add:hover {
+				font-size: 32px;
+				color: #409eff;
+				font-weight: bold;
 			}
 		}
 
-		.card-add {
-			font-size: 32px;
-			cursor: pointer;
-			display: flex;
-			justify-content: center;
-			align-items: center;
-		}
 
-		.card-item:hover {
-			border-color: #409eff;
-			background-color: #3d415a;
-		}
-
-		.card-add:hover {
-			font-size: 32px;
-			color: #409eff;
-			font-weight: bold;
-		}
 	}
 </style>
--- frontend/src/views/page/notify/index.vue
+++ frontend/src/views/page/notify/index.vue
@@ -1,100 +1,105 @@
 <template>
-	<div class="notify" v-loading="loading">
-		<div class="card-item" v-for="item in dataList">
-			<div class="card-item-top">
-				<el-image :src="`/notify/${item.method}.png`" fit="contain" style="width: 60px;height: 60px;"></el-image>
-				<div style="margin-left: 12px;">
-					<div class="card-item-user">{{item.method | notifyMethodFilter}}</div>
-					<div :class="`card-item-enable enable-${item.enable == 1 ? 'enable' : 'disable'}`">
-						{{item.enable == 1 ? '已启用' : '已禁用'}}
+	<div class="notify">
+		<div class="loading-box content-none-data" v-loading="true" v-if="loading">加载中</div>
+		<div v-else class="card-box">
+			<div class="card-item" v-for="item in dataList">
+				<div class="card-item-top">
+					<el-image :src="`/notify/${item.method}.png`" fit="contain" style="width: 60px;height: 60px;"></el-image>
+					<div style="margin-left: 12px;">
+						<div class="card-item-user">{{item.method | notifyMethodFilter}}</div>
+						<div :class="`card-item-enable enable-${item.enable == 1 ? 'enable' : 'disable'}`">
+							{{item.enable == 1 ? '已启用' : '已禁用'}}
+						</div>
 					</div>
 				</div>
+				<div class="card-item-bottom">
+					<el-button size="small" type="primary" @click="editShowDialog(item)">编辑</el-button>
+					<el-button size="small" type="success" v-if="item.enable == 0"
+						@click="enableNotify(item.id, 1)">启用</el-button>
+					<el-button size="small" type="warning" v-else @click="enableNotify(item.id, 0)">禁用</el-button>
+					<el-button size="small" type="primary" :loading="tstLoading" @click="tstCu(item)">测试</el-button>
+					<el-button size="small" type="danger" @click="delCu(item.id)">删除</el-button>
+				</div>
 			</div>
-			<div class="card-item-bottom">
-				<el-button size="small" type="primary" @click="editShowDialog(item)">编辑</el-button>
-				<el-button size="small" type="success" v-if="item.enable == 0" @click="enableNotify(item.id, 1)">启用</el-button>
-				<el-button size="small" type="warning" v-else @click="enableNotify(item.id, 0)">禁用</el-button>
-				<el-button size="small" type="primary" :loading="tstLoading" @click="tstCu(item)">测试</el-button>
-				<el-button size="small" type="danger" @click="delCu(item.id)">删除</el-button>
+			<div class="card-item card-add" @click="addShow" v-if="!loading">
+				<template v-if="dataList.length == 0">
+					暂无通知配置，请<span style="color: #409eff;">新增</span>
+				</template>
+				<span v-else>新增</span>
 			</div>
-		</div>
-		<div class="card-item card-add" @click="addShow" v-if="!loading">
-			<template v-if="dataList.length == 0">
-				暂无通知配置，请<span style="color: #409eff;">新增</span>
-			</template>
-			<span v-else>新增</span>
-		</div>
-		<el-dialog :close-on-click-modal="false" top="6vh" :visible.sync="editShow" :title="editFlag ? '编辑' : '新增'"
-			width="680px" :before-close="closeShow" :append-to-body="true">
-			<div class="elform-box">
-				<el-form :model="editData" :rules="editRule[editData.method]" ref="addRule" v-if="editShow" label-width="100px">
-					<el-form-item prop="enable" label="是否启用">
-						<el-switch v-model="editData.enable" :active-value="1" :inactive-value="0">
-						</el-switch>
-					</el-form-item>
-					<el-form-item prop="method" label="方式">
-						<el-select v-model="editData.method" @change="methodChange" style="width: 100%;">
-							<el-option :key="meItem - 1" :value="meItem - 1" :label="meItem - 1 | notifyMethodFilter"
-								v-for="meItem in 3"></el-option>
-							<!-- <el-option :key="1" :value="1" label="server酱"></el-option>
+			<el-dialog :close-on-click-modal="false" top="6vh" :visible.sync="editShow" :title="editFlag ? '编辑' : '新增'"
+				width="680px" :before-close="closeShow" :append-to-body="true">
+				<div class="elform-box">
+					<el-form :model="editData" :rules="editRule[editData.method]" ref="addRule" v-if="editShow"
+						label-width="100px">
+						<el-form-item prop="enable" label="是否启用">
+							<el-switch v-model="editData.enable" :active-value="1" :inactive-value="0">
+							</el-switch>
+						</el-form-item>
+						<el-form-item prop="method" label="方式">
+							<el-select v-model="editData.method" @change="methodChange" style="width: 100%;">
+								<el-option :key="meItem - 1" :value="meItem - 1" :label="meItem - 1 | notifyMethodFilter"
+									v-for="meItem in 3"></el-option>
+								<!-- <el-option :key="1" :value="1" label="server酱"></el-option>
 							<el-option :key="2" :value="2" label="钉钉机器人"></el-option> -->
-						</el-select>
-					</el-form-item>
-					<template v-if="editData.method == 0">
-						<el-form-item prop="params.url" label="请求地址">
-							<el-input v-model="editData.params.url" placeholder="请输入请求地址"></el-input>
-						</el-form-item>
-						<el-form-item prop="params.method" label="请求方法">
-							<el-select v-model="editData.params.method" style="width: 100%;">
-								<el-option key="POST" value="POST" label="POST"></el-option>
-								<el-option key="PUT" value="PUT" label="PUT"></el-option>
-								<el-option key="GET" value="GET" label="GET"></el-option>
 							</el-select>
 						</el-form-item>
-						<el-form-item v-if="editData.params.method != 'GET'" prop="params.contentType" label="请求体类型">
-							<el-select v-model="editData.params.contentType" style="width: 100%;">
-								<el-option key="application/json" value="application/json" label="application/json"></el-option>
-								<el-option key="application/x-www-form-urlencoded" value="application/x-www-form-urlencoded"
-									label="application/x-www-form-urlencoded"></el-option>
-							</el-select>
-						</el-form-item>
-						<el-form-item prop="params.titleName" label="标题参数名">
-							<el-input v-model="editData.params.titleName" placeholder="请输入标题参数名"></el-input>
-						</el-form-item>
-						<el-form-item prop="params.needContent" label="是否需要内容">
-							<el-select v-model="editData.params.needContent" style="width: 100%;">
-								<el-option :key="true" :value="true" label="需要"></el-option>
-								<el-option :key="false" :value="false" label="不需要"></el-option>
-							</el-select>
-						</el-form-item>
-						<el-form-item prop="params.contentName" v-if="editData.params.needContent" label="内容参数名">
-							<el-input v-model="editData.params.contentName" placeholder="请输入内容参数名"></el-input>
-						</el-form-item>
-					</template>
-					<template v-else-if="editData.method == 1">
-						<div class="tip-box">同时支持 <a href="https://sct.ftqq.com/" target="_blank">Server酱ᵀ</a>(免费5次/天)
-							与 <a href="https://sc3.ft07.com/" target="_blank">Server酱³</a>(公测不限次)</div>
-						<el-form-item prop="params.sendKey" label="SendKey">
-							<el-input v-model="editData.params.sendKey" placeholder="请输入SendKey"></el-input>
-						</el-form-item>
-					</template>
-					<template v-else-if="editData.method == 2">
-						<div class="tip-box"><a
-								href="https://open.dingtalk.com/document/orgapp/custom-bot-creation-and-installation"
-								target="_blank">配置指南</a> 安全设置请采用[自定义关键字]，关键字内容为[TaoSync]，不含中括号</div>
-						<el-form-item prop="params.url" label="WebHook">
-							<el-input v-model="editData.params.url"
-								placeholder="https://oapi.dingtalk.com/robot/send?access_token=xxxx"></el-input>
-						</el-form-item>
-					</template>
-				</el-form>
-			</div>
-			<span slot="footer" class="dialog-footer">
-				<el-button @click="closeShow">取 消</el-button>
-				<el-button type="success" :loading="tstLoading" @click="tstCu()">测 试</el-button>
-				<el-button type="primary" @click="submit" :loading="editLoading">确 定</el-button>
-			</span>
-		</el-dialog>
+						<template v-if="editData.method == 0">
+							<el-form-item prop="params.url" label="请求地址">
+								<el-input v-model="editData.params.url" placeholder="请输入请求地址"></el-input>
+							</el-form-item>
+							<el-form-item prop="params.method" label="请求方法">
+								<el-select v-model="editData.params.method" style="width: 100%;">
+									<el-option key="POST" value="POST" label="POST"></el-option>
+									<el-option key="PUT" value="PUT" label="PUT"></el-option>
+									<el-option key="GET" value="GET" label="GET"></el-option>
+								</el-select>
+							</el-form-item>
+							<el-form-item v-if="editData.params.method != 'GET'" prop="params.contentType" label="请求体类型">
+								<el-select v-model="editData.params.contentType" style="width: 100%;">
+									<el-option key="application/json" value="application/json" label="application/json"></el-option>
+									<el-option key="application/x-www-form-urlencoded" value="application/x-www-form-urlencoded"
+										label="application/x-www-form-urlencoded"></el-option>
+								</el-select>
+							</el-form-item>
+							<el-form-item prop="params.titleName" label="标题参数名">
+								<el-input v-model="editData.params.titleName" placeholder="请输入标题参数名"></el-input>
+							</el-form-item>
+							<el-form-item prop="params.needContent" label="是否需要内容">
+								<el-select v-model="editData.params.needContent" style="width: 100%;">
+									<el-option :key="true" :value="true" label="需要"></el-option>
+									<el-option :key="false" :value="false" label="不需要"></el-option>
+								</el-select>
+							</el-form-item>
+							<el-form-item prop="params.contentName" v-if="editData.params.needContent" label="内容参数名">
+								<el-input v-model="editData.params.contentName" placeholder="请输入内容参数名"></el-input>
+							</el-form-item>
+						</template>
+						<template v-else-if="editData.method == 1">
+							<div class="tip-box">同时支持 <a href="https://sct.ftqq.com/" target="_blank">Server酱ᵀ</a>(免费5次/天)
+								与 <a href="https://sc3.ft07.com/" target="_blank">Server酱³</a>(公测不限次)</div>
+							<el-form-item prop="params.sendKey" label="SendKey">
+								<el-input v-model="editData.params.sendKey" placeholder="请输入SendKey"></el-input>
+							</el-form-item>
+						</template>
+						<template v-else-if="editData.method == 2">
+							<div class="tip-box"><a
+									href="https://open.dingtalk.com/document/orgapp/custom-bot-creation-and-installation"
+									target="_blank">配置指南</a> 安全设置请采用[自定义关键字]，关键字内容为[TaoSync]，不含中括号</div>
+							<el-form-item prop="params.url" label="WebHook">
+								<el-input v-model="editData.params.url"
+									placeholder="https://oapi.dingtalk.com/robot/send?access_token=xxxx"></el-input>
+							</el-form-item>
+						</template>
+					</el-form>
+				</div>
+				<span slot="footer" class="dialog-footer">
+					<el-button @click="closeShow">取 消</el-button>
+					<el-button type="success" :loading="tstLoading" @click="tstCu()">测 试</el-button>
+					<el-button type="primary" @click="submit" :loading="editLoading">确 定</el-button>
+				</span>
+			</el-dialog>
+		</div>
 	</div>
 </template>
 
@@ -328,69 +333,81 @@
 
 	.notify {
 		box-sizing: border-box;
-		padding: 8px;
-		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(380px, 2fr));
 		width: 100%;
+		height: 100%;
 
-		.card-item {
-			background-color: #292b3c;
-			border-radius: 5px;
-			border: 1px solid;
-			border-color: transparent;
-			height: 110px;
-			margin: 8px;
-			padding: 6px;
+		.loading-box {
+			box-sizing: border-box;
+			width: 100%;
+			height: 100%;
+		}
 
-			.card-item-top {
-				display: flex;
-				align-items: center;
-				justify-content: center;
+		.card-box {
+			box-sizing: border-box;
+			padding: 8px;
+			display: grid;
+			grid-template-columns: repeat(auto-fill, minmax(380px, 2fr));
+			width: 100%;
 
-				.card-item-user {
-					font-size: 18px;
+			.card-item {
+				background-color: #292b3c;
+				border-radius: 5px;
+				border: 1px solid;
+				border-color: transparent;
+				height: 110px;
+				margin: 8px;
+				padding: 6px;
+
+				.card-item-top {
 					display: flex;
+					align-items: center;
+					justify-content: center;
+
+					.card-item-user {
+						font-size: 18px;
+						display: flex;
+					}
+
+					.card-item-enable {
+						margin-top: 6px;
+						font-weight: bold;
+					}
+
+					.enable-enable {
+						color: #67c23a;
+					}
+
+					.enable-disable {
+						color: #f56c6c;
+					}
 				}
 
-				.card-item-enable {
-					margin-top: 6px;
-					font-weight: bold;
-				}
-
-				.enable-enable {
-					color: #67c23a;
-				}
-
-				.enable-disable {
-					color: #f56c6c;
+				.card-item-bottom {
+					display: flex;
+					align-items: center;
+					justify-content: center;
+					margin-top: 12px;
 				}
 			}
 
-			.card-item-bottom {
+			.card-add {
+				font-size: 32px;
+				cursor: pointer;
 				display: flex;
-				align-items: center;
 				justify-content: center;
-				margin-top: 12px;
+				align-items: center;
 			}
-		}
 
-		.card-add {
-			font-size: 32px;
-			cursor: pointer;
-			display: flex;
-			justify-content: center;
-			align-items: center;
-		}
+			.card-item:hover {
+				border-color: #409eff;
+				background-color: #3d415a;
+			}
 
-		.card-item:hover {
-			border-color: #409eff;
-			background-color: #3d415a;
-		}
-
-		.card-add:hover {
-			font-size: 32px;
-			color: #409eff;
-			font-weight: bold;
+			.card-add:hover {
+				font-size: 32px;
+				color: #409eff;
+				font-weight: bold;
+			}
 		}
 	}
 </style>
