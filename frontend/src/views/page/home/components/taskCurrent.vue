45ce568a57f2f664bb26dd6ee52b449dae10f072
dr34m-cn <1416814478@qq.com>
2025-06-05 22:25:21 +0800

    add 部分作业详情功能添加（开发中）

--- frontend/src/views/page/home/components/taskCurrent.vue
+++ frontend/src/views/page/home/components/taskCurrent.vue
@@ -4,14 +4,31 @@
 			{{loading ? '加载中' : '作业未在进行中'}}
 		</div>
 		<div class="current-box" v-else>
-			<taskCurrentEcharts class="current-echart-box" :taskCurrent="current"></taskCurrentEcharts>
-			<div class="current-center-box">
-				当前状态：扫描{{current.scanFinish ? '完成，' : ''}}同步中<br/>
-				开始时间：{{current.createTime | timeStampFilter}}<br/>
-				持续时间：{{formatSeconds(current.duration)}}<br/>
-				同步速度(估算)：
+			<div class="current-box-top">
+				<div>当前状态：扫描{{current.scanFinish ? '完成，' : ''}}同步中</div>
+				<div>平均同步速度(估算)：{{calcSpeedAvg() | sizeFilter}}/s</div>
+				<div>持续时间：{{formatSeconds(current.duration)}}</div>
+				<div>开始时间：{{current.createTime | timeStampFilter}}</div>
 			</div>
-			<div class="current-task-box"></div>
+			<div class="current-box-bottom">
+				<taskCurrentEcharts class="current-echart-box" :taskCurrent="current"></taskCurrentEcharts>
+				<div class="current-box-task">
+					<div class="current-box-task-left">
+						<div @click="changeTaskCu(0)" :class="`task-left-item${cuTaskSelect == 0 ? ' is-current' : ''}`">
+							等待中</div>
+						<div @click="changeTaskCu(1)" :class="`task-left-item${cuTaskSelect == 1 ? ' is-current' : ''}`">
+							进行中</div>
+						<div @click="changeTaskCu(2)" :class="`task-left-item${cuTaskSelect == 2 ? ' is-current' : ''}`">
+							成功</div>
+						<div @click="changeTaskCu(7)" :class="`task-left-item${cuTaskSelect == 7 ? ' is-current' : ''}`">
+							失败</div>
+						<div @click="changeTaskCu(-1)"
+							:class="`task-left-item${cuTaskSelect == -1 ? ' is-current' : ''}`">
+							其他
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -37,6 +54,8 @@
 		data() {
 			return {
 				loading: false,
+				cuTaskSelect: 1, // 0 1 2 7 -1
+				cuTaskList: [],
 				current: {
 					'scanFinish': false,
 					'num': {
@@ -54,7 +73,7 @@
 						'other': 1
 					},
 					'createTime': 1749042892,
-					'duration': 98661,
+					'duration': 661,
 					'doingTask': [{
 						'srcPath': '/A/',
 						'dstPath': '/B/',
@@ -78,20 +97,43 @@
 			getCurrent() {
 
 			},
+			calcSpeedAvg() {
+				let doingSize = this.current.doingTask.reduce((sum, obj) => {
+					return sum + (sum, obj.fileSize * obj.progress);
+				}, 0);
+				return (this.current.size.success + doingSize) / this.current.duration;
+			},
+			changeTaskCu(status) {
+				this.cuTaskSelect = status;
+				if (status == 1) {
+					this.cuTaskList = this.current.doingTask;
+				}
+			},
 			formatSeconds(seconds) {
-			  const days = Math.floor(seconds / (24 * 3600));
-			  const hours = Math.floor((seconds % (24 * 3600)) / 3600);
-			  const minutes = Math.floor((seconds % 3600) / 60);
-			  const secs = seconds % 60;
-			  const timeUnits = [
-			    { value: days, unit: '天' },
-			    { value: hours, unit: '小时' },
-			    { value: minutes, unit: '分钟' },
-			    { value: secs, unit: '秒' }
-			  ];
-			  const nonZeroUnits = timeUnits.filter(unit => unit.value > 0);
-			  if (nonZeroUnits.length === 0) return '0秒';
-			  return nonZeroUnits.map(unit => `${unit.value}${unit.unit}`).join(' ');
+				const days = Math.floor(seconds / (24 * 3600));
+				const hours = Math.floor((seconds % (24 * 3600)) / 3600);
+				const minutes = Math.floor((seconds % 3600) / 60);
+				const secs = seconds % 60;
+				const timeUnits = [{
+						value: days,
+						unit: '天'
+					},
+					{
+						value: hours,
+						unit: '小时'
+					},
+					{
+						value: minutes,
+						unit: '分钟'
+					},
+					{
+						value: secs,
+						unit: '秒'
+					}
+				];
+				const nonZeroUnits = timeUnits.filter(unit => unit.value > 0);
+				if (nonZeroUnits.length === 0) return '0秒';
+				return nonZeroUnits.map(unit => `${unit.value}${unit.unit}`).join(' ');
 			},
 			show() {
 				this.$emit('currentChange', 300);
@@ -108,17 +150,60 @@
 		.current-box {
 			background-color: #100c2a;
 			height: calc(100% - 12px);
-			padding: 8px 0;
+			padding: 8px 10px;
 			width: 100%;
 			box-sizing: border-box;
-			display: flex;
 
-			.current-echart-box {
-				height: 100%;
-				width: 40%;
-				min-width: 390px;
-				box-sizing: border-box;
-				border-right: 1px dotted #fff;
+			.current-box-top {
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				padding: 6px 0;
+				border-bottom: 1px dotted #fff;
+
+				div {
+					width: 268px;
+				}
+			}
+
+			.current-box-bottom {
+				height: calc(100% - 36px);
+				width: 100%;
+				display: flex;
+
+				.current-echart-box {
+					height: 100%;
+					width: 40%;
+					min-width: 390px;
+					box-sizing: border-box;
+					border-right: 1px dotted #fff;
+				}
+
+				.current-box-task {
+					width: 60%;
+					box-sizing: border-box;
+					padding: 8px 12px;
+					display: flex;
+
+					.current-box-task-left {
+						width: 60px;
+						.task-left-item {
+							cursor: pointer;
+							width: 60px;
+							margin: 14px 0;
+							padding: 3px 6px 3px 0;
+							color: #4f5968;
+							text-align: right;
+							box-sizing: border-box;
+						}
+						
+						.is-current {
+							color: #409eff;
+							border-right: 3px solid #409eff;
+							background-color: rgba(64, 158, 255, 0.4);
+						}
+					}
+				}
 			}
 		}
 	}
